fbb8fc24e26ef7cc101b87410949592c9b8ed004
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2020-10-20 10:16:33 -0500

    more cued recall

--- Cued-Recall/Original/score m_h.R
+++ Cued-Recall/Original/score m_h.R
@@ -12,6 +12,26 @@ table(dat$Experiment)
 
 library(lrd)
 
+##Okay, subset by experiment
+A = subset(dat,
+           dat$Experiment == "JOL A")
+
+B = subset(dat,
+           dat$Experiment == "JOL B")
+
+C = subset(dat,
+           dat$Experiment == "JOL C")
+
+D = subset(dat,
+           dat$Experiment == "JOL D")
+
+##Next, need to sort the data and add trial numbers
+#160 different pair types
+table(A$Sub.ID) #117, 121, 125, and 129 each have 144 observations for some reason
+table(B$Sub.ID) #114, 118, 122, 126
+table(C$Sub.ID) #116, 119, 123, 127
+table(D$Sub.ID) #120, 124, 128
+
 #make sure response and key are each lowercase
 dat$Response = tolower(dat$Response)
 dat$Key = tolower(dat$Key)
