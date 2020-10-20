0330b801ff43a852db6d08804f1eca36533cb3ba
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2020-10-20 09:47:17 -0500

    working on cued recall analyses

--- Cued-Recall/Original/score m_b.R
+++ Cued-Recall/Original/score m_b.R
@@ -1,8 +1,10 @@
 ####set up####
-dat = read.csv("Maxwell Buchanan 2019 part 2.csv")
+dat = read.csv("Maxwell_Buchanan.csv")
 
 colnames(dat)[3] = "Cue"
 colnames(dat)[6] = "manually_coded"
+
+table(dat$Sub.ID)
 
 library(lrd)
 
@@ -13,8 +15,59 @@ dat$Key = tolower(dat$Key)
 #Add the trial id
 dat$Trial_id = rep(c(1:63))
 
-#Now compute the percent match
-prop_correct_cued(dat, responses = "Response", key = "Key", 
-                  id = "Sub.ID", id.trial = "Trial_id", cutoff = 0)
+#Score all the things!
+out0 = prop_correct_cued(dat, responses = "Response", key = "Key", key.trial = "Trial_id", 
+                  id = "Sub.ID", id.trial = "Trial_id", cutoff = 0, flag = T)
 
-prop_
+out1 = prop_correct_cued(dat, responses = "Response", key = "Key", key.trial = "Trial_id", 
+                  id = "Sub.ID", id.trial = "Trial_id", cutoff = 1, flag = T)
+
+out2 = prop_correct_cued(dat, responses = "Response", key = "Key", key.trial = "Trial_id", 
+                  id = "Sub.ID", id.trial = "Trial_id", cutoff = 2, flag = T)
+
+out3 = prop_correct_cued(dat, responses = "Response", key = "Key", key.trial = "Trial_id", 
+                  id = "Sub.ID", id.trial = "Trial_id", cutoff = 3, flag = T)
+
+out4 = prop_correct_cued(dat, responses = "Response", key = "Key", key.trial = "Trial_id", 
+                  id = "Sub.ID", id.trial = "Trial_id", cutoff = 4, flag = T)
+
+out5 = prop_correct_cued(dat, responses = "Response", key = "Key", key.trial = "Trial_id", 
+                  id = "Sub.ID", id.trial = "Trial_id", cutoff = 5, flag = T)
+
+####Now get all the scored stuff back into the same df####
+##First, get all the scored outputs
+scored0 = out0$DF_Scored
+scored1 = out1$DF_Scored
+scored2 = out2$DF_Scored
+scored3 = out3$DF_Scored
+scored4 = out4$DF_Scored
+scored5 = out5$DF_Scored
+
+##Okay, now need to sort the rows back into the correct order
+scored0 = scored0[order(scored0$Key), ]
+scored0 = scored0[order(scored0$Sub.ID), ]
+
+scored1 = scored1[order(scored1$Key), ]
+scored1 = scored1[order(scored1$Sub.ID), ]
+
+scored2 = scored2[order(scored2$Key), ]
+scored2 = scored2[order(scored2$Sub.ID), ]
+
+scored3 = scored3[order(scored3$Key), ]
+scored3 = scored3[order(scored3$Sub.ID), ]
+
+scored4 = scored4[order(scored4$Key), ]
+scored4 = scored4[order(scored4$Sub.ID), ]
+
+scored5 = scored5[order(scored5$Key), ]
+scored5 = scored5[order(scored5$Sub.ID), ]
+
+dat$scored0 = scored0$Scored
+dat$scored1 = scored1$Scored
+dat$scored2 = scored2$Scored
+dat$scored3 = scored3$Scored
+dat$scored4 = scored4$Scored
+dat$scored5 = scored5$Scored
+
+##Write the scored data to .csv
+write.csv(dat, file = "maxwell_buchanan scored.csv", row.names = F)
--- Cued-Recall/Original/score m_h.R
+++ Cued-Recall/Original/score m_h.R
@@ -6,6 +6,10 @@ colnames(dat)[4] = "Key"
 colnames(dat)[2] = "Sub.ID"
 colnames(dat)[6] = "manually_coded"
 
+##Get the data organized for scoring
+#need to add trial ids
+table(dat$Experiment)
+
 library(lrd)
 
 #make sure response and key are each lowercase
@@ -13,101 +17,5 @@ dat$Response = tolower(dat$Response)
 dat$Key = tolower(dat$Key)
 
 #Now compute the percent match
-maxhuff_matched = percent_match(dat$Response, key = dat$Key, id = dat$Sub.ID)
-
-####Score the data####
-#going to use 10 cutoff points: 100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50
-
-#first add percent match
-dat$percent_match = maxhuff_matched$percent_match
-
-#100%
-score_recall(maxhuff_matched, set.cutoff = 1)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$one_hundred = output$scored
-
-#95%
-score_recall(maxhuff_matched, set.cutoff = .95)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$ninety_five = output$scored
-
-#90%
-score_recall(maxhuff_matched, set.cutoff = .90)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$ninety= output$scored
-
-#85%
-score_recall(maxhuff_matched, set.cutoff = .85)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$eightyfive= output$scored
-
-#80%
-score_recall(maxhuff_matched, set.cutoff = .80)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$eighty= output$scored
-
-#75%
-score_recall(maxhuff_matched, set.cutoff = .75)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$seventy_five = output$scored
-
-#70%
-score_recall(maxhuff_matched, set.cutoff = .70)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$seventy = output$scored
-
-#65%
-score_recall(maxhuff_matched, set.cutoff = .65)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$sixty_five = output$scored
-
-#60%
-score_recall(maxhuff_matched, set.cutoff = .60)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$sixty = output$scored
-
-#55%
-score_recall(maxhuff_matched, set.cutoff = .55)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$fifty_five = output$scored
-
-#50%
-score_recall(maxhuff_matched, set.cutoff = .50)
-
-#now add the scored data to the original file
-output = read.csv("output.csv") #load in the scored data
-
-dat$fifty = output$scored
-
-#write.csv(dat,
-#        file = "max_huff processed.csv", row.names = F)
+prop_correct_cued(dat, responses = "Response", key = "Key", 
+                  id = "Sub.ID", id.trial = "Sub.ID", cutoff = 0)
