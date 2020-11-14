64728185a5a3d0c9fd09a7ba06380152bf9413c4
npm27 <nick.maxwell61@yahoo.com>
2020-11-14 15:46:07 -0600

    Update Free Recall Example.R

--- Manuscript/Free-Example/Free Recall Example.R
+++ Manuscript/Free-Example/Free Recall Example.R
@@ -4,4 +4,14 @@ library(lrd)
 dat = read.csv("free_example_data.csv")
 key = read.csv("free_example_key.csv")
 
+##Convert from wide to long
 arranged = arrange_data(dat, responses = "Response", id = "Sub.ID", sep = ", ")
+
+##Score free recall data
+scored_free = prop_correct_free(arranged,
+                                responses = "response",
+                                key = key$Answer_Key,
+                                id = "Sub.ID",
+                                cutoff = 1)
+
+View(scored_free$DF_Scored)
